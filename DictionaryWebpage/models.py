1466265e8f5a4dd6deac825373195051049b120d
Rafisto <1.rafal.wlodarczyk@gmail.com>
2022-03-29 21:28:38 +0200

    29.03/3 - DATABASE INTEGRATION (10% DONE)

--- DictionaryWebpage/models.py
+++ DictionaryWebpage/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+
+# FROM dictionary.sql
+# `word` varchar(25) NOT NULL, - word
+# `wordtype` varchar(20) NOT NULL, - part of speech
+# 'definition` text NOT NULL - word definition
+
 class Word(models.Model):
     word = models.CharField(max_length=30)
-    definition = models.CharField(max_length=500)
+    wordtype = models.CharField(max_length=30)
+    definition = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.word
--- DictionaryWebpage/views.py
+++ DictionaryWebpage/views.py
@@ -1,6 +1,7 @@
 from django import views
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
+from DictionaryWebpage import models
 
 
 @csrf_exempt
@@ -8,5 +9,7 @@ def index(request):
     if request.method == "GET":
         word = request.GET.get('word', '')
         if 'word' in request.GET:
-            print(request.GET['word'])
-        return render(request, 'index.html', {'word': word})
+            b = models.Word.objects.filter(word=word)
+            return render(request, 'index.html', {'word': word, 'definition': b[0]})
+        else:
+            return render(request, 'index.html', {'word': word})
